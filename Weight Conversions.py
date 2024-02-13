cd59c16ba1bbd780c0130edc7c22eb24dcf7738b
Nicholas <158377391+nicholaslwood@users.noreply.github.com>
2024-02-13 15:53:47 -0500

    Add files via upload

---
+++ Weight Conversions.py
@@ -0,0 +1,12 @@
+# I created this program to help myself to convert Kilograms to Pounds and vice versa
+
+kg = int(input("Kilograms to Pounds. Please enter value in kilograms: "))
+pound = 2.205
+
+result = kg * pound 
+
+print(f"{result} Pounds")
+
+print("Pounds to Kilograms. Please enter value in pounds.")
+x = float(input('Pounds: '))
+print(x * .454)
